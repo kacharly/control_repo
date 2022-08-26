48c3351a70e87ffd8fb8ba83231fb93c9249e2a7
kacharly <karol.charly@gmail.com>
2022-08-26 16:53:29 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCds+2JuOQkpZ7iSuoiEYvHdMwAVUqtJSUuGq42QywFZJWhY1wCkuvMmWOFcoV9FiYV4eGrPaD/HJI1cN1dyYXy9CkQUfP/nOpl8CJH14Z+ga8lGqvxHayYRbLrPQGTOHsGhtEkIztB6Wov1eZ68QNGqIw+rUX5HgkWfdYHqajM62PvMbqpvc9KJmLCFfSXYLwzUzla8BbiecPVUhCBVyHQdL5ASsnvEtDOPS9SsNm9HLgOop8BXbqGWyC3B/TrHQGeAaJFXEWqKLvFW8NGAaOTey1DbXfCUO/TZLXDkN/kT/ByHzCaywHjxA58e2tXMOhXEVMMQVh6VL4WpkPD/XPv root@master.puppet.vm',
 	}  
 }
